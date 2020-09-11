2134474b41afb8b4208db58af2721021ee7c8cf5
Charles Collins <coll5631@stthomas.edu>
2020-09-11 12:34:48 -0500

    Added company list and functions for getting attributes

--- Google_Companies.py
+++ Google_Companies.py
@@ -1,9 +1,46 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
-params = {'q': 'Blossom Chevrolet'}
-r = requests.get('http://www.google.com/search', params = params)
-rtext = BeautifulSoup(r.text, 'html.parser')
+searched = 'Blossom Chevrolet'
 
-print(rtext.findAll('span', {'class': 'BNeawe tAd8D AP7Wnd'}))
-print(rtext.findAll('span', {'class': 'BNeawe s3v9rd AP7Wnd'})   )
+params = {'q': searched}
+r = requests.get('http://www.google.com/search', params=params)
+r_html = BeautifulSoup(r.text, 'html.parser')
+
+
+def get_gresult_title(bs4_html):
+    return (bs4_html.find('div', {'id': 'main'}).find('div', {'class': 'ZINbbc xpd O9g5cc uUPGi'}).find('h3', {
+        'class': "zBAuLc"}).text)
+
+
+def get_gresult_address(bs4_html):
+    for el in bs4_html.find('div', {'id': 'main'}).find('div', {'class': 'ZINbbc xpd O9g5cc uUPGi'}).findAll('div', {
+        'class': 'BNeawe s3v9rd AP7Wnd'}):
+        txt = el.text
+        if txt.find('Address: ') == 0:
+            return (txt.replace('Address: ', ''))
+
+
+def get_gresult_phone(bs4_html):
+    for el in r_html.find('div', {'id': 'main'}).find('div', {'class': 'ZINbbc xpd O9g5cc uUPGi'}).findAll('div', {
+        'class': 'BNeawe s3v9rd AP7Wnd'}):
+        txt = el.text
+        if txt.find('Phone: ') == 0:
+            return txt.replace('Phone: ', '')
+
+
+def get_gattribute(bs4_html, fun):
+    try:
+        return fun(bs4_html)
+    except:
+        print(searched, ' Attribute Not Found')
+        return ''
+
+
+print(pd.DataFrame(data={
+    'Searched': [searched],
+    'Title': [get_gresult_title(r_html)],
+    'Address: ': [get_gresult_address(r_html)],
+    'Phone: ': [get_gresult_phone(r_html)]
+}))
