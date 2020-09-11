9f3bea7e7b069010021f8e4353e0848ad808098b
Charles Collins <coll5631@stthomas.edu>
2020-09-11 12:47:55 -0500

    Added max loops for testing

--- Google_Companies.py
+++ Google_Companies.py
@@ -14,6 +14,10 @@ def get_gresult_title(bs4_html):
         'class': "zBAuLc"}).text)
 
 
+def get_gwebsite(bs4_html):
+    return bs4_html.find('div', {'class': 'BNeawe UPmit AP7Wnd'}).text
+
+
 def get_gresult_address(bs4_html):
     for el in bs4_html.find('div', {'id': 'main'}).find('div', {'class': 'ZINbbc xpd O9g5cc uUPGi'}).findAll('div', {
         'class': 'BNeawe s3v9rd AP7Wnd'}):
@@ -23,7 +27,7 @@ def get_gresult_address(bs4_html):
 
 
 def get_gresult_phone(bs4_html):
-    for el in r_html.find('div', {'id': 'main'}).find('div', {'class': 'ZINbbc xpd O9g5cc uUPGi'}).findAll('div', {
+    for el in bs4_html.find('div', {'id': 'main'}).find('div', {'class': 'ZINbbc xpd O9g5cc uUPGi'}).findAll('div', {
         'class': 'BNeawe s3v9rd AP7Wnd'}):
         txt = el.text
         if txt.find('Phone: ') == 0:
@@ -34,28 +38,34 @@ def get_gattribute(bs4_html, fun):
     try:
         return fun(bs4_html)
     except:
-        print(searched, ' Attribute Not Found')
+        print('Attribute Not Found')
         return ''
 
 
 company_list = pd.read_csv('company_list.csv')
-all_company_data = []
+loops = 0
+max_pages = 3000
 
 for company in company_list.loc[:, 'Company']:
+    loops += 1
     print(company)
     try:
         searched = get_page(company)
-        all_company_data.append(pd.DataFrame(data={
+        all_company_data = (pd.DataFrame(data={
             'Searched': [company],
             'Title': [get_gattribute(searched, get_gresult_title)],
             'Address: ': [get_gattribute(searched, get_gresult_address)],
-            'Phone: ': [get_gattribute(searched, get_gresult_phone)]
+            'Phone: ': [get_gattribute(searched, get_gresult_phone)],
+            'Website: ': [get_gattribute(searched, get_gwebsite)]
         }))
+        with open('company_search_results.csv', 'a') as f:
+            all_company_data.to_csv(f, header=False)
     except:
         print(company, ' could not be retrieved')
 
     time.sleep(.5)
+    if loops > max_pages:
+        break
 
-
-all_company_data = pd.concat(all_company_data, axis=0)
-all_company_data.to_csv('company_search_results.csv')
+# all_company_data = pd.concat(all_company_data, axis=0)
+# all_company_data.to_csv('company_search_results.csv')
